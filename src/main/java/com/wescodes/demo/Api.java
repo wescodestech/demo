afd0ddb26ae877c0238be3806ec954fd4ee80fc9
Wesley Howell <wesleyrhowell@gmail.com>
2021-05-31 00:49:03 -0500

    Add Add Ten

--- src/main/java/com/wescodes/demo/Api.java
+++ src/main/java/com/wescodes/demo/Api.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Api {
+    @GetMapping("/{num}")
+    public Sum addTen(@PathVariable("num") int num){
+        return new Sum(num + 10);
+    }
+
     @GetMapping("/{num1}/{num2}")
     public Sum getSum(@PathVariable("num1") int num1,
                       @PathVariable("num2") int num2){
