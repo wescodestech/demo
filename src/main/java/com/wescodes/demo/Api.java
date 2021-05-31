c357ca988b4ce8d20bf83ba436a52992c3c8baea
Wesley Howell <wesleyrhowell@gmail.com>
2021-05-31 00:45:40 -0500

    Add Sum Addition Endpoint

--- src/main/java/com/wescodes/demo/Api.java
+++ src/main/java/com/wescodes/demo/Api.java
@@ -1,10 +1,17 @@
 package com.wescodes.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Api {
+    @GetMapping("/{num1}/{num2}")
+    public Sum getSum(@PathVariable("num1") int num1,
+                      @PathVariable("num2") int num2){
+        return new Sum(num1 + num2);
+    }
+
     @GetMapping("/api")
     public String GetApi() {
         return "Hello WesCodes World!";
---
+++ src/main/java/com/wescodes/demo/Sum.java
@@ -0,0 +1,13 @@
+package com.wescodes.demo;
+
+public class Sum {
+    public int sum;
+
+    public Sum(int sum){
+        this.sum = sum;
+    }
+
+    public void setSum(int sum){
+        this.sum = sum;
+    }
+}
